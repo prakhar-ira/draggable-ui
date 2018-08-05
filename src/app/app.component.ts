c91cb099731ef35d83e82e1229152288b18678cb
Prakhar Dixit <pdixit@clinicalplus.ca>
2018-08-05 19:29:47 +0530

    draggable-ui using angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,32 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  droppedData = '';
+  counter = 0;
+  counter1 = 0;
+  dragEnd(event) {
+    this.droppedData = '';
+  console.log('Element was dragged', event);
+  }
+
+  refresh() {
+    this.counter = 0;
+    this.counter1 = 0;
+  }
+  onDrop(data: any): void {
+    this.droppedData = data.dropData;
+    switch (this.droppedData) {
+      case 'Tea' : this.counter ++;
+                    break;
+      case 'Coffee' : this.counter1 ++;
+                    break;
+      default: break;
+    }
+    setTimeout(() => {
+      this.droppedData = '';
+    }, 2000);
+  }
 }
